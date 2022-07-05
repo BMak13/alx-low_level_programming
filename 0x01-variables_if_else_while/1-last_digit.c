90b916d694d08743b56a55c058e92d64eba1ac4d
Brandon Makhubo <brandon.makhubo@gmail.com>
2022-07-05 05:05:21 +0200

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,10 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main- assign a random number to the variable n each time it is executed. 
- * Description : it prints the correct string
+ * Description: it prints the correct string
  * Return: Always 0 (Success)
  */
 
