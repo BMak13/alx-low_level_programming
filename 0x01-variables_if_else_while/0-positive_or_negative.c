459459fa39b67e2ae85c45e3d241d51c11f84c60
Brandon Makhubo <brandon.makhubo@gmail.com>
2022-07-05 04:55:26 +0200

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,21 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
- *  Main -assigns a random number to int n eeverytime
- *
- * Description: it exectutes and prints it answer
- *
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
  * Return: Always 0 (Success)
- */
-/*
- * Only output documentation for the given function(s)
- *
- *  betty style doc for function main goes there
- *
- *  main - assigns a random number to int n everytime
- *
- *  Description:  it will execute and print answer
  */
 
 int main(void)
