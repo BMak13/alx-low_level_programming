7541c584f545ad53a6a03c400abb2d1873250de5
Brandon Makhubo <brandon.makhubo@gmail.com>
2022-07-06 05:48:55 +0200

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
-* main - print lower case letters
+* print_alphabet -prints  lower case letters
 * Description: indicating the letters to be input in lower case 
 * Return: Always 0 (sucess)
 */
 void print_alphabet(void)
 {
 	int i;
+
 	for (i = 97; i <= 122; i++)
 	{
 	_putchar(i);
