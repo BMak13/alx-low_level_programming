7d6c30823187b242bd4e218b301ccd918c1aeb1e
Brandon Makhubo <brandon.makhubo@gmail.com>
2022-07-07 02:31:46 +0200

    Task 3

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,13 +2,13 @@
 #define MAIN_H
 
 /**
- * main - main function block
- * Description: print functions
+ * main - function block
+ * Description: print below functions
  * Return: Always 0
  */
 
-int _putchar(char c);
+int _putchar(void);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-
-#endif 
+int _islower(int);
+#endif
