99e7d796e506334757fff2b7cee262dcd77d555e
Brandon Makhubo <brandon.makhubo@gmail.com>
2022-07-09 14:35:12 +0200

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,17 +2,19 @@
 
 /**
  * main- prints all single digit numbers of base 10 starting from 0
+ *Description: this shall print base numbers 
  * Return : Always 0
  */
 
 int main(void)
 {
 	int c;
+
 	for (c = 48; c <= 57 ; c++)
 	{
 	putchar(c);
 	}
 	putchar(10);/* this is an ascii code for a new line */
 
-	return(0);
+	return (0);
 }
