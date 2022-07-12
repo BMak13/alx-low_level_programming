17a13307d55451ef524936223adc1ca32332a847
Brandon Makhubo <brandon.makhubo@gmail.com>
2022-07-13 00:56:14 +0200

    Task 0

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+#Pointers,arrays and strings
+
+# ALX low level programming introduction to pointers, arays and stings
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Main - function block 
+ * Description: to reference c files
+ * Return: always 0
+ */
+
+void reset_to_98(int *n);
+#endif 
