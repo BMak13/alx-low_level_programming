7c6e582ec9546fa026c7b9e5e65a05f4c329a244
Brandon Makhubo <brandon.makhubo@gmail.com>
2022-07-13 03:13:06 +0200

    Task 3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,8 +6,9 @@
  * Description: to reference c files
  * Return: always 0
  */
-
+void _putchar();
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 #endif 
