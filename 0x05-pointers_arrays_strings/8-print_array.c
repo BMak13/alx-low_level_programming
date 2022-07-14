17d571db62de6fee34ac2c4c897d390c03a47a77
Brandon Makhubo <brandon.makhubo@gmail.com>
2022-07-14 04:41:38 +0200

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - Prints array
+ * @a : integer
+ * @n : integer
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i == n - 1)
+			continue;
+		printf(", ");
+	}
+	printf("\n");
+}
