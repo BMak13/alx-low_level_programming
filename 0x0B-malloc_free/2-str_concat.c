7d4712d89483af9086e0fd4110aef69facb1690c
Brandon Makhubo <brandon.makhubo@gmail.com>
2022-07-28 01:16:07 +0200

    Task 2 hot fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,27 +11,18 @@ char *str_concat(char *s1, char *s2)
 {
 	int i = 0, j = 0, l = 0, k = 0;
 	char *s;
-	/*
-	 * conditions of the function
-	 */
+	
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	/*
-	 *string 1 -[variable i] i will increase to a value 1++
-	 *string 2 [variable j] j will increase to a value 1++
-	 */
+	
 	while (s1[i])
 		i++;
 	while (s2[j])
 		j++;
 
 	l = i + j;
-
-	/*
-	 * Memory allocation
-	 */
 
 	s = (char *)malloc(l * sizeof(char) + 1);
 	if (s == NULL)
