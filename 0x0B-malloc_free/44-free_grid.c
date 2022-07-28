acf7abfc5b4012b26395816e1b0ce7e31badac22
Brandon Makhubo <brandon.makhubo@gmail.com>
2022-07-28 03:29:34 +0200

    Task 4

---
+++ 0x0B-malloc_free/44-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *free_grid- remove momory allocation previously made
+ *@grid: grid memory block to be cleared
+ *@height: height of matrix
+ *Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int o;
+
+	for (o = 0; o < height; o++)
+		free(grid[o]);
+	free(grid);
+}
+ 
