5c4d03218d131745a90410b100d2ec70b2b6f2a3
BMak13 <brandon.makhubo@hotmail.com>
2023-05-18 18:26:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
-/*
- *main - Entry point
+/**
+ *main - Entry point of file
  *
- * return - 0
+ *Desciption: 'Print sentence'
+ *@puts - print sentences
+ *
+ *Return: always 0 (success)
+ *
  */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle);
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
