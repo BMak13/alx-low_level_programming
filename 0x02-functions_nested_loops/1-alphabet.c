bf16db4f42cf8d4dbd970fdc15675e03dba0b937
BMak13 <brandon.makhubo@hotmail.com>
2023-05-24 15:12:39 -0700

    1aa

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * print_alphabet - print the alphabet
  * @c - holds the alphabet to be printed
  * Return: 0
  */
+
 void print_alphabet(void)
 {
 	while (char c = 'a'; c <= 'z'; c++);
-	{
-		putchar(c);
-	}
+	
+	putchar(c);
 	putchar('\n');
+	return (0);
 }
