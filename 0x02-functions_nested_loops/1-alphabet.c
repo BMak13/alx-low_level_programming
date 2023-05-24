9183531b00b18d4229a3f62bfe6697f72c75a1f0
BMak13 <brandon.makhubo@hotmail.com>
2023-05-24 15:14:48 -0700

    1a[

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,16 @@
 /**
  * print_alphabet - print the alphabet
  * @c - holds the alphabet to be printed
- * Return: 0
+ * 
  */
 
 void print_alphabet(void)
 {
-	while (char c = 'a'; c <= 'z'; c++);
+	char c;
+
+	while (c = 'a'; c <= 'z'; c++);
 	
 	putchar(c);
 	putchar('\n');
-	return (0);
+
 }
