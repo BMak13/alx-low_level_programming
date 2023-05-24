e82ea6683839572a185fb1e33bb9a0f38e385b18
BMak13 <brandon.makhubo@hotmail.com>
2023-05-23 18:39:30 -0700

    1-

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - print the alphabet
+ * @c - holds the alphabet to be printed
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+	int c = 97;
+
+	for (c = 97; c <= 122; c++);
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
