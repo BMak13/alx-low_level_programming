fb614d03648a7baaaa751b438fb82e3f100dc698
BMak13 <brandon.makhubo@hotmail.com>
2023-05-23 17:42:47 -0700

    0-

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+/** 
+ * main: use putchar in function
+ *
+ * Description: print with _putchar
+ * @_putchar: prints letter
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
