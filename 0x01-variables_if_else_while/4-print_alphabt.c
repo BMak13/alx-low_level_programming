d819b0f3e7bab689d7c04db73386e7a4b4e4a9d6
BMak13 <brandon.makhubo@hotmail.com>
2023-05-25 15:59:29 -0700

    4a

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print letters 
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	while (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch = 'q' || ch = 'e')
+		{
+			ch++
+		}
+		putchar(ch);
+	}
+	putchar('\n');
+}
