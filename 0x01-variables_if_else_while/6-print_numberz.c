6d3a5a2bbf59c95cbd38fe6f51e5743810032b97
BMak13 <brandon.makhubo@hotmail.com>
2023-05-25 16:28:07 -0700

    6p

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
-/** 
+/**
  * main - print digit
  *
  * Return: 0
@@ -9,7 +8,7 @@ int main(void)
 {
 	int i;
 
-	for (i = '0'; i <= 9; i++)
+	for (i = '48'; i <= 57; i++)
 	{
 		putchar(i);
 	}
