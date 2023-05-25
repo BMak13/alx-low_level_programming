c7b479fe67ad248c418f6cb6722a0c506b3331e6
BMak13 <brandon.makhubo@hotmail.com>
2023-05-25 15:27:09 -0700

    letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print alphabet
+ *
+ * Return: 0
+ */
+
+int main()
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{ 
+		putchar(l)
+	}
+	return(0);
+}
