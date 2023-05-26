7fe7081e6f68f1287f2f728b48041008f4e14d46
BMak13 <brandon.makhubo@hotmail.com>
2023-05-26 00:57:42 -0700

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -8,10 +8,12 @@
 
 void positive_or_negative(int i)
 {
-	int i;
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
 
-	i = 0;
 
-	positive_or_negative(i);
-	return (0);
 }
