b2759cd0cc2f3dd6b2ae5a4635d88615d813a49c
BMak13 <brandon.makhubo@hotmail.com>
2023-05-25 17:30:54 -0700

    0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - postive or negative
+ *
+ * Return: 0
+ */
+
+void positive_or_negative(int i)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
