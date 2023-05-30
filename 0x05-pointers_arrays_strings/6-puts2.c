2351e86dfe90d419faeb2e3eb0f5fae4c837d4ac
BMak13 <brandon.makhubo@hotmail.com>
2023-05-30 14:31:54 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character
+ * @str: the string
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int o;
+
+	while (str[o] != '\0')
+	{
+		o++;
+	}
+
+	for (i = 0; i < o; i += 2)
+	{
+		putchar(str[i];
+	}
+	putchar('\n');
+}
