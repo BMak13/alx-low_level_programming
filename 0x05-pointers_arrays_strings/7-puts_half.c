92d83a5a169d5593ea046e9fa8c1dc91ee1096b9
BMak13 <brandon.makhubo@hotmail.com>
2023-05-30 14:51:00 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - Prints half of the string
+ * @str: string to print
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int n = 0;
+	int b;
+
+	while (str[n] != '\n')
+	{
+		n++
+	}
+	if (n % 2 == 1)
+	{
+		b = (n -1) /2;
+		b += 1;
+	}
+	else 
+	{
+		b = n / 2;
+	}
+
+	for (b < n; b++)
+	{
+		putchar(str[b]);
+	}
+	putchar('\n');
+}
