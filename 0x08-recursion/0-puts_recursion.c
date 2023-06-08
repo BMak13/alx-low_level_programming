ad107015be09bcd414c8dc303dc4e4a06a7c0c7a
BMak13 <brandon.makhubo@hotmail.com>
2023-06-08 15:38:08 -0700

    0 rec

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - print puts
+ * @s: the string to be printed
+ * Return: 0
+ * \
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+	putchar(*s);
+	_puts_recursion(s + 1);
+	}
+	else 
+		putchar('\n');
+}
