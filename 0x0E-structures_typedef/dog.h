872833940d48ff8653217135b33525cdbb5f025d
BMak13 <brandon.makhubo@hotmail.com>
2023-06-19 19:13:37 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name: name 
+ * @age: age
+ * @owner: owner
+ * Description: dog struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct my_dog;
+
+#endif
